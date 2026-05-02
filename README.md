4a4fb190e820f203ba3b4c0880159c83c2e4fab0
Himanshu Mahara <himanshumahara@gmail.com>
2026-05-02 05:58:23 +0530

    first basic setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# First Tutorial
+
+In this tutorial, you will learn how to:
+
+- Install the OpenAI package
+- Install `config` and `dotenv` npm packages
+- Call the OpenAI API with a prompt and get a response
+
+## Quickstart Documentation
+
+https://developers.openai.com/api/docs/quickstart
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Initial Setup");
+import * as dotenv from "dotenv";
+dotenv.config();
+import config from "config";
+import OpenAI from "openai";
+
+//console.log(config.get("openai.key"));
+//console.log(config.get("ai.model"));
+
+
+const client = new OpenAI();
+const model = config.get("ai.model");
+
+const response = await client.responses.create({
+    model,
+    input: "Write a one-sentence bedtime story about a unicorn."
+});
+
+console.log(response.output_text);
